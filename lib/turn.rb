d23ae775772697b1b0b29b961800cf83ca333ed5
Tanesha Goodman <t24good@icloud.com>
2021-01-09 16:00:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,11 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
+
+def valid_move?(board, index)
+  index.between(0, 8) &&!position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "0"
+end
