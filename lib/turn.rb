ceec71d063964fa9523b7ca6dd3014408a7eace4
Tanesha Goodman <t24good@icloud.com>
2021-01-09 15:51:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def display_board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "#{board[O]} | #{board[O]} | #{board[O]} "
+  puts "#{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "#{board[X]} | #{board[X]} | #{board[X]} "
+  puts "#{board[6]} | #{board[7]} | #{board[8]} "
 end
